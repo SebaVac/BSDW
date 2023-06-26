bb617a12af0866303f81afd79f478bf021329d53
Felipe Godoy <80706667+Pipasoo@users.noreply.github.com>
2023-06-25 21:02:53 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Registro from './pages/Registro';
 import Inicio from './pages/Inicio';
-
 function App() {
   return (
     <>
